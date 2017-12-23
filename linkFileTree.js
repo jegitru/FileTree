79491e5f8963eb25ad18574b08c59bd9feac23dd
jegitru@email.com <jegitru@gmail.com>
2017-12-23 14:07:21 +0700

    link jQuery

--- linkFileTree.js
+++ linkFileTree.js
@@ -1,63 +1,67 @@
-linkFileTree = function(id){
-    
-    // Get all elements
-    function getElements(){
-        return document.getElementById(id).parentNode.getElementsByTagName('a');
-    };
-    
-    function forEachElements(callback){
-        var elements = getElements();
-        for ( var i = 0; i < elements.length; i++ ) {
-            var result = callback.call(elements[i]);
-            if ( typeof(result) != 'undefined' ) {
-                return result;
-            }
-        }
-    };
-    
-    function getListElement(elem){
-      
-        while ( elem = elem.nextSibling ) {
-            if ( elem.nodeType == 1 ) {
-                if ( elem.nodeName == 'UL' ) {
-                    break;
+(function($){
+  
+    window.linkFileTree = function(id){
+        
+        // Get all elements
+        function getElements(){
+            return document.getElementById(id).parentNode.getElementsByTagName('a');
+        };
+        
+        function forEachElements(callback){
+            var elements = getElements();
+            for ( var i = 0; i < elements.length; i++ ) {
+                var result = callback.call(elements[i]);
+                if ( typeof(result) != 'undefined' ) {
+                    return result;
                 }
             }
-        }
+        };
         
-        return elem;
-    };
-    
-    forEachElements(function(){
-      
-        this.addEventListener('click', function(e){
-
-            if ( this.getAttribute('data-type') == 'folder' ) {
-              
-                var listElement = getListElement(this);
-                
-                if ( listElement ) {
-                  
-                    var style = listElement.style;
-                    
-                    if ( style.display == 'none' ) {
-
-                        style.display = '';
-                        this.parentNode.setAttribute('data-opened', '1');
-                      
-                    } else {
-
-                        style.display = 'none';
-                        this.parentNode.setAttribute('data-opened', '0');
+        function getListElement(elem){
+          
+            while ( elem = elem.nextSibling ) {
+                if ( elem.nodeType == 1 ) {
+                    if ( elem.nodeName == 'UL' ) {
+                        break;
                     }
                 }
             }
             
-            if ( e.preventDefault ) {
-                e.preventDefault();
-            }
+            return elem;
+        };
+        
+        forEachElements(function(){
+          
+            this.addEventListener('click', function(e){
 
-            return false;
+                if ( this.getAttribute('data-type') == 'folder' ) {
+                  
+                    var listElement = getListElement(this);
+                    
+                    if ( listElement ) {
+                      
+                        var style = listElement.style;
+                        
+                        if ( style.display == 'none' ) {
+
+                            style.display = '';
+                            this.parentNode.setAttribute('data-opened', '1');
+                          
+                        } else {
+
+                            style.display = 'none';
+                            this.parentNode.setAttribute('data-opened', '0');
+                        }
+                    }
+                }
+                
+                if ( e.preventDefault ) {
+                    e.preventDefault();
+                }
+
+                return false;
+            });
         });
-    });
-};
+    };
+
+}(jQuery));
