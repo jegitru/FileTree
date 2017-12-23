db30a73c8ca5723697be76220c9ca31378b1fb30
jegitru@email.com <jegitru@gmail.com>
2017-12-23 10:46:21 +0700

    Add file linkFileTree.js

---
+++ linkFileTree.js
@@ -0,0 +1,63 @@
+linkFileTree = function(id){
+    
+    // Get all elements
+    function getElements(){
+        return document.getElementById(id).parentNode.getElementsByTagName('a');
+    };
+    
+    function forEachElements(callback){
+        var elements = getElements();
+        for ( var i = 0; i < elements.length; i++ ) {
+            var result = callback.call(elements[i]);
+            if ( typeof(result) != 'undefined' ) {
+                return result;
+            }
+        }
+    };
+    
+    function getListElement(elem){
+      
+        while ( elem = elem.nextSibling ) {
+            if ( elem.nodeType == 1 ) {
+                if ( elem.nodeName == 'UL' ) {
+                    break;
+                }
+            }
+        }
+        
+        return elem;
+    };
+    
+    forEachElements(function(){
+      
+        this.addEventListener('click', function(e){
+
+            if ( this.getAttribute('data-type') == 'folder' ) {
+              
+                var listElement = getListElement(this);
+                
+                if ( listElement ) {
+                  
+                    var style = listElement.style;
+                    
+                    if ( style.display == 'none' ) {
+
+                        style.display = '';
+                        this.setAttribute('data-opened', '1');
+                      
+                    } else {
+
+                        style.display = 'none';
+                        this.setAttribute('data-opened', '0');
+                    }
+                }
+            }
+            
+            if ( e.preventDefault ) {
+                e.preventDefault();
+            }
+
+            return false;
+        });
+    });
+};
